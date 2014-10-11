5118fb4a64724059dfdff46e54c0d4562d94ef1a
oleg <obutovich@gmail.com>
2014-10-11 13:40:51 +0200

    fixed shasum

--- Casks/psi-plus-webkit.rb
+++ Casks/psi-plus-webkit.rb
@@ -1,6 +1,6 @@
 class PsiPlusWebkit < Cask
   version '0.16.19-webkit'
-  sha256 'd2dfd89a7b9dbdfbe5a55e6054d71319f05335bd'
+  sha256 '1cff35c40d664d2887a8fbd7747beb781836a28d43b6bf8bcfa80945c736ab39'
 
   url 'http://downloads.sourceforge.net/project/psiplus/Mac-OS-X/psi-plus-0.16.19-webkit-macosx.dmg'
 

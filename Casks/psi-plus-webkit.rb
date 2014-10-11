0785b9031dcefd04dc303ab35a0b8d686cda882e
oleg <obutovich@gmail.com>
2014-10-11 13:39:03 +0200

    psi+ (webkit)

---
+++ Casks/psi-plus-webkit.rb
@@ -0,0 +1,8 @@
+class PsiPlusWebkit < Cask
+  version '0.16.19-webkit'
+  sha256 'd2dfd89a7b9dbdfbe5a55e6054d71319f05335bd'
+
+  url 'http://downloads.sourceforge.net/project/psiplus/Mac-OS-X/psi-plus-0.16.19-webkit-macosx.dmg'
+
+  app 'Psi+.app'
+end
